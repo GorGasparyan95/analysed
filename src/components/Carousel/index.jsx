8cd388da0e2549d115ef2fd3add7d419759bc29d
Gor <gorgasparyann95@gmail.com>
2022-05-02 01:14:21 +0300

    swiperjs

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,68 +1,113 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, Navigation } from 'swiper'
 import { ReactComponent as Left } from '../../assets/left.svg'
 import { ReactComponent as Right } from '../../assets/rigth.svg'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
 
+
+SwiperCore.use([Navigation, Pagination])
 
 const Carousel = () => {
-  const [slide, setSlide] = useState(0)
+  const navPrevButton = useRef(null)
+  const navNextButton = useRef(null)
 
 
-  const listRef = useRef(null);
-
-  const scrollLeft = () => {
-    if (listRef.current && slide >= 0) {
-      listRef.current.scrollBy({
-        top: 0,
-        left: -360,
-        behavior: "smooth",
-      });
+  const onBeforeInit = (Swiper) => {
+    if (typeof Swiper.params.navigation !== 'boolean') {
+      const navigation = Swiper.params.navigation
+      navigation.prevEl = navPrevButton.current
+      navigation.nextEl = navNextButton.current
     }
-    if (slide === 0) return
-    setSlide(slide - 1)
-  };
+  }
+  // const listRef = useRef(null);
+
+  // const scrollLeft = () => {
+  //   if (listRef.current && slide >= 0) {
+  //     listRef.current.scrollBy({
+  //       top: 0,
+  //       left: -360,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  //   if (slide === 0) return
+  //   setSlide(slide - 1)
+  // };
 
 
-  const scrollRight = () => {
-    if (listRef.current && slide !== 2) {
-      listRef.current.scrollBy({
-        top: 0,
-        left: 360,
-        behavior: "smooth",
-      });
-    }
-    if (slide === 2) return
-    setSlide(slide + 1)
-  };
+  // const scrollRight = () => {
+  //   if (listRef.current && slide !== 2) {
+  //     listRef.current.scrollBy({
+  //       top: 0,
+  //       left: 360,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  //   if (slide === 2) return
+  //   setSlide(slide + 1)
+  // };
 
 
   return (
     <div className='carousel_wrapper'>
-      <div className='carousel_container'>
-        <Left onClick={scrollLeft} />
-        <div className='items_container' ref={listRef}>
-          <div className='single_item'>
-            <img src="./assets/images/azgayin.png" />
-          </div>
-          <div className='single_item'>
-            <img src="./assets/images/Moris.png" />
-          </div>
-          <div className='single_item'>
-            <img src="./assets/images/Soft.png" />
-          </div>
-          <div className='single_item'>
-            <img src="./assets/images/azgayin.png" />
-          </div>
-          <div className='single_item'>
-            <img src="./assets/images/azgayin.png" />
-          </div>
-        </div>
-        <Right onClick={scrollRight} />
-      </div>
-      <div className='radio_wrapper'>
+
+      <button ref={navPrevButton} className="carousel_btn">
+        <Left />
+      </button>
+
+
+
+      <Swiper
+        onBeforeInit={onBeforeInit}
+        spaceBetween={10}
+        slidesPerView={3}
+        pagination={true}
+      // autoplay={{
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // }}
+      >
+
+
+        <SwiperSlide>
+
+          <img src="./assets/images/azgayin.png" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img src="./assets/images/Moris.png" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img src="./assets/images/Soft.png" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img src="./assets/images/azgayin.png" />
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img src="./assets/images/azgayin.png" />
+
+        </SwiperSlide>
+      </Swiper >
+      <button ref={navNextButton} className="carousel_btn">
+        <Right />
+      </button>
+
+
+
+
+      {/* <div className='radio_wrapper'>
         <div type="radio" className={slide === 0 ? "dactive dot" : "dot"} />
         <div type="radio" className={slide === 1 ? "dactive dot" : "dot"} />
         <div type="radio" className={slide === 2 ? "dactive dot" : "dot"} />
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/pages/Homepage/MainFooter.jsx
+++ src/pages/Homepage/MainFooter.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from "../../components/Footer";
-import Carousel from "../../components/Carousel";
+import Carousel from '../../components/Carousel';
 
 
 const MainFooter = () => {
   return (
     <div className='main_footer'>
-      {/* <Carousel /> */}
+      <Carousel />
       <Footer />
     </div>
   )
